bc2fd6b2ecdc78aaf33c1d36dc3930d560542615
kahataku <takuya.kahara@gmail.com>
2023-07-20 12:15:50 +0900

    メッセージ削除・削除取消機能追加

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -5,6 +5,7 @@ use Inertia\Inertia;
 use App\Http\Requests;
 use App\Models\Message;
 use App\Models\Room;
+use App\Models\Participant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -26,29 +27,47 @@ class ChatController extends Controller
 
         // チャット一覧を取得
         $message = Message::select(
-            'users.id',
-            'users.name',
+            'users.id as user_id',
+            DB::raw(
+                "CASE
+                    WHEN participants.user_id IS NULL THEN 'unknown'
+                    ELSE users.name
+                END AS user_name"
+            ),
+            'messages.id',
             'messages.contents',
+            'messages.is_delete',
             DB::raw('DATE_FORMAT(messages.created_at, "%Y/%m/%d") as send_at'),
             DB::raw('TIME_FORMAT(messages.created_at, "%k:%i") as send_time'),
         )
-        ->leftJoin('users', 'messages.user_id', '=', 'users.id')
+        ->join('users', 'messages.user_id', '=', 'users.id')
+        ->leftJoin('participants', function($join) use($roomId) {
+            $join->on('messages.user_id', '=', 'participants.user_id')
+            ->where('participants.room_id', $roomId);
+        })
         ->where('messages.room_id', $roomId)
         ->orderby('messages.created_at', 'asc')
         ->get();
 
         // ルーム情報を取得
-        $roomInfo = Room::select(
-            'id',
-            'name'
-        )
-        ->where('id', $roomId)
+        $roomInfo = Room::where('id', $roomId)
         ->first();
+
+        // チャットのメンバーを取得
+        $members = Participant::select(
+            'users.id',
+            'users.name'
+        )
+        ->join('users', 'participants.user_id', '=', 'users.id')
+        ->where('participants.room_id', $roomId)
+        ->orderby('participants.created_at', 'asc')
+        ->get();
 
         return Inertia::render('Chat', [
             'user_id' => Auth::id(),
             'messages' => $message,
-            'room_info' => $roomInfo
+            'room_info' => $roomInfo,
+            'members' => $members
         ]);
     }
 
@@ -60,7 +79,7 @@ class ChatController extends Controller
     public function create(Request $request) {
         // メッセージとルームIDが空の場合、処理を行わない
         if (empty($request->send_message) || empty($request->room_id)) {
-            return to_route('chat');
+            return to_route('chat', ["room_id" => $request->room_id]);
         }
         // 登録処理
         $created_message_id = Message::insertGetId([
@@ -73,6 +92,27 @@ class ChatController extends Controller
 
         // イベント（Pusher）
         broadcast(new MessageControl($created_message_id))->toOthers();
+
+        return to_route('chat', ["room_id" => $request->room_id]);
+    }
+
+    /**
+     * チャットメッセージの削除・再表示処理
+     * 
+     * @param Request $request
+     */
+    public function deleteMessage(Request $request) {
+        // idとルームIDが空の場合、処理を行わない
+        if (empty($request->id) || empty($request->room_id)) {
+            return to_route('chat', ["room_id" => $request->room_id]);
+        }
+
+        // is_deleteを更新
+        Message::where('id', $request->id)
+        ->update([
+            'is_delete' => $request->is_delete,
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
 
         return to_route('chat', ["room_id" => $request->room_id]);
     }
--- resources/js/Pages/Chat.vue
+++ resources/js/Pages/Chat.vue
@@ -3,11 +3,14 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head, router, useForm} from '@inertiajs/vue3';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import { nextTick, onMounted, ref } from 'vue';
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 
 defineProps({
     user_id: Number,
     messages: Array,
-    room_info: Object
+    room_info: Object,
+    members: Array
 });
 
 const form = useForm({
@@ -57,6 +60,51 @@ const submit = () => {
         }
     });
 }
+
+// メッセージ編集要素を表示
+const showEditChatButton = (e) => {
+    const edit_chat_button = e.target.firstElementChild;
+    if (edit_chat_button.classList.contains("hidden")) {
+        edit_chat_button.classList.remove("hidden");
+    }
+}
+
+// メッセージ編集要素を非表示
+const hideEidtChatButton = (e) => {
+    e.target.firstElementChild.classList.add("hidden");
+}
+
+// メッセージ削除・再表示データ送信処理
+const changeIsDelete = (message_id, is_delete) => {
+    const room_id = document.getElementById("roomId").value;
+    router.put(route('chat.delete'), {
+        id: message_id,
+        room_id: room_id,
+        is_delete: is_delete
+    }, {
+        onSuccess: () => {
+            scrollEnd();
+            preventScroll.value = window.scrollY;
+        }
+    });
+}
+
+// メッセージ削除処理
+const deleteMessage = (e) => {
+    const message_id = e.target.dataset.id;
+    changeIsDelete(message_id, 1);
+}
+
+// メッセージ再表示処理
+const showMessage = (e) => {
+    const message_id = e.target.dataset.id;
+    changeIsDelete(message_id, 0);
+}
+
+const withdraw = () => {
+    const room_id = document.getElementById("roomId").value;
+    router.delete(route('room.withdraw', {id: room_id}));
+}
 </script>
 
 <template>
@@ -64,29 +112,83 @@ const submit = () => {
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ room_info.name }}</h2>
-            <input type="hidden" :value="room_info.id" id="roomId">
+            <div class="flex justify-between item-center">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ room_info.name }}</h2>
+                <input type="hidden" :value="room_info.id" id="roomId">
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="ml-3 relative">
+                        <Dropdown align="right" width="48">
+                            <template #trigger>
+                                <span class="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                    >
+                                        メンバー：{{ members.length }}人
+                                        <svg
+                                            class="ml-2 -mr-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </template>
+
+                            <template #content>
+                                <div v-for="member in members" :key="member.id">
+                                    <p class="px-4 py-2 text-gray-700">{{ member.name }}</p>
+                                </div>
+                                <button v-if="room_info.created_user !== user_id" @click="withdraw" class="px-4 py-2 text-red-600 border-t border-solid border-slate-200 w-full text-sm">退会する</button>
+                            </template>
+                        </Dropdown>
+                    </div>
+                </div>
+            </div>
         </template>
 
         <div class="py-12" id="chatScroll">
-            <!-- <div v-if="messages.length"> -->
-                <div v-for="(message, index) in messages" :key="index">
-                    <div v-if="index === 0 || message.send_at !== messages[index - 1].send_at">
-                        <p class="text-center">{{ message.send_at }}</p>
+            <div v-for="(message, index) in messages" :key="index">
+                <p v-if="index === 0 || message.send_at !== messages[index - 1].send_at" class="text-center mb-2">{{ message.send_at }}</p>
+                <template v-if="message.is_delete">
+                    <div class="sm:px-6 lg:px-8 mb-4 flex justify-center item-center">
+                        <p class="text-center">{{ message.user_name }}がメッセージを削除しました</p>
+                        <button v-if="message.user_id === user_id" class="ml-5 text-xs text-sky-500" :data-id="message.id" @click="showMessage">取消</button>
                     </div>
-                    <div class="max-w-100 sm:px-6 lg:px-8 flex" v-bind:class="{'flex-row-reverse' : message.id === user_id}">
-                        <div>
-                            <p>{{ message.name }}</p>
+                </template>
+                <template v-else>
+                    <div class="max-w-100 sm:px-6 lg:px-8 mb-4 flex" v-bind:class="{'flex-row-reverse' : message.user_id === user_id}">
+                        <div class="relative">
+
+                            <p v-if="message.user_id !== user_id">{{ message.user_name }}</p>
                             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                                <div class="p-6 text-gray-900">
-                                    <p class="whitespace-pre-wrap">{{ message.contents }}</p>
-                                </div>
+                                <template v-if="message.user_id === user_id">
+                                    <div
+                                        class="p-6 text-gray-900 whitespace-pre-wrap"
+                                        @mouseenter="showEditChatButton"
+                                        @mouseleave="hideEidtChatButton"
+                                    >
+                                        {{ message.contents }}
+                                        <div class="absolute -top-4 right-0 text-xs hidden text-red-600">
+                                            <button @click="deleteMessage" :data-id="message.id">削除</button>
+                                        </div>
+                                    </div>
+                                </template>
+                                <template v-else>
+                                    <div class="p-6 text-gray-900 whitespace-pre-wrap">{{ message.contents }}</div>
+                                </template>
                             </div>
                             <p class="text-right">{{ message.send_time}}</p>
                         </div>
                     </div>
-                </div>
-            <!-- </div> -->
+                </template>
+            </div>
             <div class="fixed bottom-0 w-full py-1.5" ref="formTextArea">
                 <form @submit.prevent="submit" class="sm:px-6 lg:px-8 flex items-center">
                     <textarea class="w-11/12 resize-none" cols="10" rows="1" v-model="form.send_message"></textarea>
